b50f6762cd51ae3eb4fa4e038b8786b213799245
Ivan Dyachkov <ivan.dyachkov@klarna.com>
2015-07-07 22:43:36 +0200

    move server port in config

--- frfpg.js
+++ frfpg.js
@@ -1,9 +1,10 @@
 'use strict';
 (function(){
  module.exports ={
-		user     : 'frf_secret',
-		database : 'frf_secret',
-		ssl      : false, //consider makin it true for a remote server
-		host     : 'localhost'
+		user        : 'frf_secret',
+		database    : 'frf_secret',
+		ssl         : false, //consider making it true for a remote server
+		host        : 'localhost',
+		server_port : 3030
 };
 }());
